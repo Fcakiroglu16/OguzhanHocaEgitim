ec964e8c4c1015905a92e64aea14d373dcc81609
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-06-25 21:34:29 +0300

    Add HybridCache and extend Redis support in CacheService
    Added Microsoft.Extensions.Caching.Hybrid for hybrid caching. Extended CacheService with methods for Redis lists, sets, sorted sets, and hashes, including generic serialization. Registered and configured HybridCache in Program.cs. Added endpoints for Redis and hybrid cache operations. Registered CacheService as a singleton.

--- CacheLesson.API/CacheService.cs
+++ CacheLesson.API/CacheService.cs
@@ -1,4 +1,6 @@
-﻿using CacheLesson.API.Services;
+﻿using System.Text.Json;
+using CacheLesson.API.Services;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
 using StackExchange.Redis;
 
 namespace CacheLesson.API
@@ -9,12 +11,134 @@ namespace CacheLesson.API
 
         public async Task SetString(string key, string value)
         {
-            await _db.StringSetAsync(key, value);
+            await _db.StringSetAsync(key, value, TimeSpan.FromMinutes(5), When.Always);
         }
+
+        public async Task SetString<T>(string key, T value)
+        {
+            var json = System.Text.Json.JsonSerializer.Serialize(value);
+            await _db.StringSetAsync(key, json, TimeSpan.FromMinutes(5), When.Always);
+        }
+
+
+        public async Task<T> GetString<T>(string key)
+        {
+            var json = await _db.StringGetAsync(key);
+            if (json.IsNullOrEmpty)
+            {
+                return default!;
+            }
+
+            return System.Text.Json.JsonSerializer.Deserialize<T>(json.ToString())!;
+        }
+
 
         public async Task<string?> GetString(string key)
         {
             return await _db.StringGetAsync(key);
+        }
+
+
+        public async Task AddList(string key, string item)
+        {
+            //LinkedList<string> names = new LinkedList<string>();
+            //var ahmetNode=  names.AddFirst("ahmet");
+            //names.AddFirst("mehmet");
+            //names.AddAfter(ahmetNode, "hasan");
+
+
+            //await _db.ListInsertAfterAsync(key, item, item);
+
+            await _db.KeyExpireAsync(key, TimeSpan.FromMinutes(5));
+            await _db.ListRightPushAsync(key, item);
+        }
+
+        public async Task RemoveList(string key, string item)
+        {
+            await _db.ListRemoveAsync(key, item);
+        }
+
+
+        public async Task AddSet(string key, string item)
+        {
+            await _db.SetAddAsync(key, item);
+        }
+
+        public async Task RemoveSet(string key, string item)
+        {
+            await _db.SetRemoveAsync(key, item);
+        }
+
+        public async Task AddSortedSet(string key, string item, double score)
+        {
+            string name = "ahmet";
+
+
+            await _db.SortedSetAddAsync(key, item, score);
+        }
+
+        public async Task RemoveSortedSet(string key, string item)
+        {
+            await _db.SortedSetRemoveAsync(key, item);
+        }
+
+        public async Task<List<string>> GetSortedList(string key)
+        {
+            var list = await _db.SortedSetRangeByScoreAsync(key, 1, 100, Exclude.None, Order.Descending, 0, -1);
+            return list.Select(x => x.ToString()).ToList();
+        }
+
+        public async Task AddHash(string key, string field, string value)
+        {
+            await _db.HashSetAsync(key, field, value);
+        }
+
+        public async Task RemoveHash(string key, string field)
+        {
+            await _db.HashDeleteAsync(key, field);
+        }
+
+        public async Task<string?> GetHash(string key, string field)
+        {
+            return await _db.HashGetAsync(key, field);
+        }
+
+
+        public async Task AddProductList()
+        {
+            var producsRepository = new ProductRepository();
+
+            var products = await producsRepository.GetProductsAsync();
+
+            foreach (var product in products)
+            {
+                _db.ListRightPush("products", System.Text.Json.JsonSerializer.Serialize(product));
+            }
+        }
+
+        public async Task AddProductHash()
+        {
+            var producsRepository = new ProductRepository();
+
+            var products = await producsRepository.GetProductsAsync();
+
+            foreach (var product in products)
+            {
+                _db.HashSet("products", product.Id.ToString(), System.Text.Json.JsonSerializer.Serialize(product));
+            }
+        }
+
+        public async Task<Product?> GetProduct(int id)
+        {
+            var productAsString = _db.HashGet("products", id.ToString());
+
+            return JsonSerializer.Deserialize<Product>(productAsString.ToString());
+        }
+
+        public async Task AddUserToHash()
+        {
+            _db.HashSet("user:1500", "name", "John Doe");
+            _db.HashSet("user:1500", "email", "john.doe@example.com");
         }
     }
 }
--- CacheLesson.API/Program.cs
+++ CacheLesson.API/Program.cs
@@ -1,5 +1,6 @@
 using CacheLesson.API;
 using CacheLesson.API.Services;
+using Microsoft.Extensions.Caching.Hybrid;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,17 @@ builder.AddServiceDefaults();
 builder.Services.AddOpenApi();
 builder.Services.AddMemoryCache();
 builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddHybridCache(options =>
+{
+    options.DefaultEntryOptions = new HybridCacheEntryOptions()
+    {
+        Expiration = TimeSpan.FromMinutes(10),
+        LocalCacheExpiration = TimeSpan.FromMinutes(5)
+    };
+});
+
+
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
@@ -21,6 +33,7 @@ builder.Services.AddScoped<ProductServiceAsInMemoryCache>();
 builder.Services.AddScoped<ProductServiceAsDistributedCache>();
 
 builder.Services.AddSingleton<RedisService>();
+builder.Services.AddSingleton<CacheService>();
 var app = builder.Build();
 
 
@@ -35,6 +48,50 @@ app.MapPost("/products", async (ProductServiceAsDistributedCache productService,
     await productService.AddProductAsync(product);
     return Results.Ok(product);
 });
+
+app.MapGet("/add-list", async (CacheService cacheService) =>
+{
+    await cacheService.AddList("my-list", "item1");
+    await cacheService.AddList("my-list", "item2");
+    await cacheService.AddList("my-list", "item3");
+    return Results.Ok("Items added to the list.");
+});
+app.MapGet("/add-sorted-set", async (CacheService cacheService) =>
+{
+    await cacheService.AddSortedSet("my-sorted-set", "item1", 1);
+    await cacheService.AddSortedSet("my-sorted-set", "item2", 50);
+    await cacheService.AddSortedSet("my-sorted-set", "item3", 100);
+    return Results.Ok("Items added to the sorted set.");
+});
+app.MapGet("/get-sorted-set", async (CacheService cacheService) =>
+{
+    var sortedSet = await cacheService.GetSortedList("my-sorted-set");
+    return Results.Ok(sortedSet);
+});
+
+app.MapGet("/add-products-to-list", async (CacheService cacheService) =>
+{
+    await cacheService.AddUserToHash();
+    return Results.Ok("Products added to the list.");
+});
+
+
+app.MapGet("/set-hybrid-cache", async (HybridCache hybridCache) =>
+{
+    await hybridCache.SetAsync("users-count", 10);
+    return Results.Ok();
+});
+app.MapGet("/get-hybrid-cache", async (HybridCache hybridCache) =>
+{
+    var count = await hybridCache.GetOrCreateAsync("users-count", async cancellationToken =>
+    {
+        var count = 50; // db.users.count
+        return count;
+    });
+    return Results.Ok(count);
+});
+
+
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
