a9342fec0456a2c1ea72a1c4cdd985a7c65d0166
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-07-14 14:37:35 +0300

    Update OpenAPI packages and refine project dependencies
    Updated .csproj files to include Microsoft.OpenApi 2.7.5 for improved OpenAPI support. Set SQL Server container lifetime to Persistent in AppHost.cs and commented out an alternative configuration. Cleaned up appsettings.Development.json by removing unused sections. Removed references to unused projects in OguzhanHocaEgitim.AppHost.csproj to streamline dependencies.

--- OguzhanHocaEgitim.AppHost/AppHost.cs
+++ OguzhanHocaEgitim.AppHost/AppHost.cs
@@ -9,10 +9,15 @@ var app2 = builder.AddProject<Projects.AppDocker2_API>("appdocker2-api");
 
 var redisPasswordParameter = builder.AddParameter("redispassword", "Password12*");
 
-var sqlSever = builder.AddSqlServer("sqlserver");
+var sqlSever = builder.AddSqlServer("sqlserver").WithLifetime(ContainerLifetime.Persistent);
 
 
 var redis = builder.AddRedis("redis").WithPassword(redisPasswordParameter);
+
+
+//builder.AddContainer("sql-server", "mcr.microsoft.com/mssql/server:2025-latest").WithEnvironment("SA_PASSWORD",
+//    "YourStrong!Passw0rd").WithEnvironment("ACCEPT_EULA", "Y")
+//    .WithEndpoint(port: 1450, targetPort: 1433, name: "tcp");
 
 
 app1.WithReference(app2).WaitFor(app2).WithReference(sqlSever).WaitFor(sqlSever);
