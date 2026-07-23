3b5d150eac6b40730753ab85d723f0102125da8f
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-07-23 13:14:41 +0300

    Refactor Create method in UserService to return Task.CompletedTask

--- QueueSystemConsoleApp/Services/UserService.cs
+++ QueueSystemConsoleApp/Services/UserService.cs
@@ -6,13 +6,13 @@ public class UserService(UserPublisher userPublisher)
 {
   //SRP  1 +2
 
-    public async Task Create()
+    public   Task Create()
     {
         Console.WriteLine("user created.");
         // create user  =1 +1+1
         userPublisher.CreateUser("ahmet16");
         Console.WriteLine("user created2.");
-
+        return Task.CompletedTask;
 
     }
 }
